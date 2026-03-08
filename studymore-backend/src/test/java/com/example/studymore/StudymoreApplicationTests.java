cca3b041839682a113a125af57ebc95342b5612b
tunar-12 <tunarb2020@gmail.com>
2026-03-08 16:46:33 +0300

    Separate JavaFX frontend and Spring Boot backend into Monorepo, add Maven wrapper

---
+++ studymore-backend/src/test/java/com/example/studymore/StudymoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studymore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
