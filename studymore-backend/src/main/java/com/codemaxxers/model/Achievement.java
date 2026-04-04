9e760ee84e660a29575019d10f472cc11c498a2b
tunar-12 <tunarb2020@gmail.com>
2026-04-04 14:19:59 +0300

    Achievement model in backend fixed

--- studymore-backend/src/main/java/com/codemaxxers/model/Achievement.java
+++ studymore-backend/src/main/java/com/codemaxxers/model/Achievement.java
@@ -11,56 +11,48 @@ public class Achievement {
     private Long achievementId;
 
     private String title;
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
     @Enumerated(EnumType.STRING)
     private AchievementType type;
 
     private int targetValue;
     private int reward;
-    public int getReward() {
-        return reward;
-    }
-
-    public void setReward(int reward) {
-        this.reward = reward;
-    }
 
     private String iconPath;
 
-    public String getIconPath() {
-        return iconPath;
-    }
-
-    public void setIconPath(String iconPath) {
+    public Achievement() {}
+ 
+    public Achievement(AchievementType type, String title, String description,
+                       int targetValue, int reward, String iconPath) {
+        this.type = type;
+        this.title = title;
+        this.description = description;
+        this.targetValue = targetValue;
+        this.reward = reward;
         this.iconPath = iconPath;
     }
 
-    public boolean checkCompletion(User user) {
-        return getProgress(user) >= this.targetValue;
+    public Long getAchievementId()                        { return achievementId; }
+    public AchievementType getType()                      { return type; }
+    public void setType(AchievementType type)             { this.type = type; }
+    public String getDescription()                        { return description; }
+    public void setDescription(String description)        { this.description = description; }
+    public int getTargetValue()                           { return targetValue; }
+    public void setTargetValue(int targetValue)           { this.targetValue = targetValue; }
+    public int getCoinReward()                            { return reward; }
+    public void setCoinReward(int reward)             { this.reward = reward; }
+    public String getIconPath()                           { return iconPath; }
+    public void setIconPath(String iconPath)              { this.iconPath = iconPath; }
+    public String getTitle()                              { return title; }
+    public void setTitle(String title)                    { this.title = title; }
+    
+    public boolean isCompleted(int currentProgress) {
+        return currentProgress >= targetValue;
     }
-
-    public void awardReward(User user) {
-        //TODO
-    }
-
-    public int getProgress(User user) {
-        return 1; //TODO
+    public int getProgressPercentage(int currentProgress) {
+        if (targetValue == 0) return 100;
+        return Math.min(100, (int) ((currentProgress / (double) targetValue) * 100));
     }
 }
